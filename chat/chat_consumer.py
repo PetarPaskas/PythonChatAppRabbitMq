f15d46ae3b2e9f88997205d25a74074dcc093c1b
PetarPaskas <petar.paskas@hotmail.com>
2024-11-09 17:50:17 +0100

    AddedConsumerAbstractAndConsumerProducerBaselineLogic

---
+++ chat/chat_consumer.py
@@ -0,0 +1,9 @@
+from comms.consumer import Consumer
+
+class ChatConsumer(Consumer):
+    def __init__(self, consumer_name):
+        super().__init__(consumer_name)
+
+    def consume(self, message_headers, message_body):
+        print(f'{self.consumer_name} listening: ')
+        print(f'body: {message_body}')
---
+++ comms/consumer/consumer.py
@@ -0,0 +1,32 @@
+
+import pika
+from abc import ABC, abstractmethod  
+connection_parameters = pika.ConnectionParameters('localhost')
+
+class Consumer(ABC):
+    connection = pika.BlockingConnection(connection_parameters)
+
+    def __init__(self, consumer_name):
+        self.consumer_name = consumer_name
+        self.__setup_channel_and_queue(consumer_name)
+        self.start_consuming()
+
+    @abstractmethod
+    def consume(self, message_headers, message_body):
+        pass 
+
+    def start_consuming(self):
+        self.channel.basic_consume(self.consumer_name, auto_ack=True, on_message_callback=self.__listen)
+        self.channel.start_consuming()
+
+    def stop_consuming(self):
+        self.channel.stop_consuming()
+
+    def __setup_channel_and_queue(self, queue_name):
+        self.channel = Consumer.connection.channel()
+        self.channel.queue_declare(queue_name)
+
+    def __listen(self, ch, method, properties, body):
+        self.consume(properties, body)
+
+    
